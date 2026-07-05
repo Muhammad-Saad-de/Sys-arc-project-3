c5c52921b5c31925537020bc5aa0a96af1588eb4
Saad <musa00008@stud.uni-saarland.de>
2026-07-05 22:06:08 +0200

    Fix jobA/jobB: check a0==0 for exec success (upstream skeleton fix per CMS)

--- programs/user_programs/jobA.s
+++ programs/user_programs/jobA.s
@@ -13,11 +13,10 @@
     li a7, 0x0A000000   # syscall number for create process
     li a6, 0x0
 
-    la a0, dummy_job    # load address of dummy_job
 job_loop:
-    la t0, dummy_job    # check whether previous job creation was successful (redundant in first iteration)
     beq t1, zero, exit_job # if no more dummy jobs to create,
-    bne a0, t0, failure
+    bne a0, zero, failure
+    la a0, dummy_job    # load address of dummy_job
     ecall               # create dummy job
     addi t1, t1, -1     # decrement the number of dummy jobs
     j job_loop          # repeat until all dummy jobs are created
